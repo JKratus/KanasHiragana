302c3e23ce0c09b4f5c4f1747ae1fbbc99aee35e
Jnourry <jordan.nourry@gmail.com>
2020-03-21 23:49:24 +0100

    refacto(kana): Rename kana translator

--- src/main/java/io/shodo/kata/kanas/domain/KanaTranslator.java
+++ src/main/java/io/shodo/kata/kanas/domain/KanaTranslator.java
@@ -3,17 +3,17 @@ package io.shodo.kata.kanas.domain;
 import io.shodo.kata.kanas.annotations.DomainService;
 
 @DomainService
-public class KanasTranslator {
-  private final KanaDictionary kanasDictionary;
+public class KanaTranslator {
+  private final KanaDictionary kanaDictionary;
   private final KanaSyllablesSplitter kanaSyllablesSplitter;
 
-  public KanasTranslator(KanaDictionary kanasDictionary, KanaSyllablesSplitter kanaSyllablesSplitter) {
-    this.kanasDictionary = kanasDictionary;
+  public KanaTranslator(KanaDictionary kanaDictionary, KanaSyllablesSplitter kanaSyllablesSplitter) {
+    this.kanaDictionary = kanaDictionary;
     this.kanaSyllablesSplitter = kanaSyllablesSplitter;
   }
 
   public Kanas translate(String word) {
     final Syllables syllables = kanaSyllablesSplitter.split(word);
-    return Kanas.of(syllables.map(kanasDictionary::correlation));
+    return Kanas.of(syllables.map(kanaDictionary::correlation));
   }
 }
--- src/test/java/io/shodo/kata/kanas/KanaTranslatorTest.java
+++ src/test/java/io/shodo/kata/kanas/KanaTranslatorTest.java
@@ -2,43 +2,43 @@ package io.shodo.kata.kanas;
 
 import io.shodo.kata.kanas.domain.KanaDictionary;
 import io.shodo.kata.kanas.domain.KanaSyllablesSplitter;
+import io.shodo.kata.kanas.domain.KanaTranslator;
 import io.shodo.kata.kanas.domain.Kanas;
-import io.shodo.kata.kanas.domain.KanasTranslator;
 import io.shodo.kata.kanas.infra.KanasHiraganaSyllabary;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class KanasTranslatorTest {
+public class KanaTranslatorTest {
 
   private final KanaDictionary kanaDictionary = new KanasHiraganaSyllabary();
   private final KanaSyllablesSplitter kanaSyllablesSplitter = new KanaSyllablesSplitter(kanaDictionary);
-  private final KanasTranslator kanasTranslator = new KanasTranslator(kanaDictionary, kanaSyllablesSplitter);
+  private final KanaTranslator kanaTranslator = new KanaTranslator(kanaDictionary, kanaSyllablesSplitter);
 
   @Test
   void should_translate_syllable_to_kana() {
-    Kanas kanas = kanasTranslator.translate("a");
+    Kanas kanas = kanaTranslator.translate("a");
 
     assertThat(kanas).isEqualTo(Kanas.of("あ"));
   }
 
   @Test
   void should_translate_two_syllables_to_kanas() {
-    Kanas kanas = kanasTranslator.translate("koni");
+    Kanas kanas = kanaTranslator.translate("koni");
 
     assertThat(kanas).isEqualTo(Kanas.write("こに"));
   }
 
   @Test
   void should_translate_three_syllables_with_syllable_of_3_chars_to_kanas() {
-    Kanas kanas = kanasTranslator.translate("konichi");
+    Kanas kanas = kanaTranslator.translate("konichi");
 
     assertThat(kanas).isEqualTo(Kanas.write("こにち"));
   }
 
   @Test
   void should_translate_complex_word() {
-    Kanas kanas = kanasTranslator.translate("konichiwa");
+    Kanas kanas = kanaTranslator.translate("konichiwa");
 
     assertThat(kanas).isEqualTo(Kanas.write("こにちわ"));
   }
